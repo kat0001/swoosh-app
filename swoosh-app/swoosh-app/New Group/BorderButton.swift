2d1a54eb84d8040f1b877dc58b39d675ff29efac
Kasia <katarzyna_michalska@icloud.com>
2019-12-29 09:13:10 +0100

    the welcome screen

---
+++ swoosh-app/swoosh-app/New Group/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  swoosh-app
+//
+//  Created by Katarzyna Michalska on 29/12/2019.
+//  Copyright © 2019 Kasia. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
